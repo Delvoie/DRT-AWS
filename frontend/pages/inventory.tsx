89233089b460c4de8f78707d9bce2f1dc057af1f
Lucas Delvoie <arozegames@gmail.com>
2026-03-14 15:25:28 -0400

    inat

---
+++ frontend/pages/inventory.tsx
@@ -0,0 +1,27 @@
+/*
+contains parts in a list of cards 
+
+parts card should have:
+- part name
+- part id 
+- status
+- category
+- view details
+- edit part 
+
+
+*/
+
+export defult function inventory(){
+
+
+
+    return(
+
+    )
+}
+
+const handleaddpart = () => {
+    name: ${part.name}
+
+}
