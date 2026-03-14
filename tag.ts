f0b3f78b2485494311d1af7cd712b00dc1bc845e
Julien <d8adp001@gmail.com>
2026-03-14 14:12:51 -0400

    stuff

---
+++ tag.ts
@@ -0,0 +1,34 @@
+/*
+
+
+Maintenence Tracker
+
+-MIN/MAX
+
+Part Tracker
+Early Warning System + Button
+
+- Rust
+- Maintenence
+- Track Parts
+
+They are not collecting DATA on maintenence data. We need to collect the data on these thing's
+If they were collecting data on the parts therefore it would be a simple executiong of outsourcing
+
+
+
+Give 
+
+- Allow them to add notes to any given part
+
+
+Use Case:
+
+Air conditioner conditions. 
+
+ {
+
+
+    }
+
+*/
